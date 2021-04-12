0a049e20334ed334c99f3e9bf8947f9d38c65f81
unknown <willferreirajr@icloud.com>
2021-04-11 21:22:13 -0300

    modificando o repositório

--- landpage-mari/src/Pages/Home/styled.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components"
-
-export const Container = styled.div `
-    display: flex;
-    max-width: 100vw;
-    height: 100vh;
-    background-color: pink;
-`
--- landpage-mari/src/Routes/Coordinator.js
+++
@@ -1,7 +0,0 @@
-export const goToHome = (history) => {
-    history.push("/")
-}
-
-export const goToBack = (history) => {
-    history.goBack()
-}
